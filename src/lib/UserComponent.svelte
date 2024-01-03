6f29b02f4e4423fbef794a0d9506308cf47f376c
Lasha Porchkhidze <lashapor@gmail.com>
2024-01-04 00:12:58 +0400

    installed tailwind properly

---
+++ src/lib/UserComponent.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<h1 class="bg-red-300 text-4xl">user</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-
+    import UserComponent from "$lib/UserComponent.svelte";
 </script>
 
-<h1>Hello TeamTime</h1>
+<h1 class="text-3xl">Hello TeamTime</h1>
+
+<UserComponent />
